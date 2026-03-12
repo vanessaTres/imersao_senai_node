a58dd799367bd0e83494eabb725fc4ea6340b6fc
VANESSA ANTONELLA ARAÚJO TRES <vanessa_tres@estudante.sesisenai.org.br>
2026-03-12 12:25:32 -0300

    jbdusg

--- server.js
+++ server.js
@@ -1,50 +1,85 @@
-import fastify from 'fastify'
-import { Pool } from 'pg'
-
+import Fastify from 'fastify';
+import { Pool } from 'pg';
 
 const sql = new Pool({
-    user:"postgres",
+    user: "postgres",
     password: "senai",
     host: "localhost",
     port: 5432,
-    database: "receitas"
+    database: "receitas" 
+})
+
+const servidor = Fastify();
+
+servidor.post('/login', async (request, reply) => {
+    const body = request.body;
+    if(!body || !body.email || !body.senha){
+        reply.status(400).send({error:"email e senha obrigatórios!"})
+    }
+
+const resultado = await sql.query('select * from usuario where email = $1 AND senha = $2', [body.email, body.senha])
+
+if(resultado.rows.length === 0){
+    reply.status(401).send({message: "Usuário ou senha inválidos!", login: false})
+} else if (resultado.rows.length === 1){
+    reply.status(200).send({message: "usuario logado", login: true})
+}
 
 })
 
-const servidor = fastify();
+
 
 servidor.get('/usuarios', async () => {
-    const resultado= await sql.query('select * from usuario')
-    return resultado.rows    
+    const resultado = await sql.query('select * from usuario')
+    return resultado.rows
+    
 })
 
-servidor.post("/usuarios", async (request,reply) => {
-    const body = request.body
+servidor.post('/usuarios', async (request, reply) => {
+    const body = request.body;
 
-    if(!body || !body.nome || !body.senha){
-        return reply.status(400).send({message:"nome e senha são obrigatórios"})
-        }
-    const resultado = await sql.query('INSERT INTO USUARIO (nome, senha) VALUES ($1, $2)', [body.nome, body.senha])
-    return 'usuario Cadastrado'
+    if (!body || !body.nome || !body.senha || !body.email) {
+        return reply.status(400).send({
+            message: "nome, email e senha são obrigatórios"
+        })
+    }
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha, email) VALUES ($1, $2, $3)', [body.nome, body.senha, body.email])
+    reply.status(201).send({message: 'Usuário Criado!'})
 })
 
-
-
-servidor.put("/usuarios/:id", async (request,reply) => {
+servidor.put('/usuarios/:id', async (request, reply) => {
     const body = request.body;
     const id = request.params.id;
-    const resultado = await sql.query('UPDATE USUARIO SET nome= $1, senha=$2 WHERE id= $3', [body.nome, body.senha, id])
-    return 'usuario Alterado'
+
+            if (!body || !body.nome || !body.senha || !body.email) {
+        return reply.status(400).send({
+            message: "nome, email e senha são obrigatórios"
+        })
+    } else if (!id){
+        return reply.status(400).send({
+            message:"Faltou o ID!"
+        })
+    }
+
+    const usuario = await sql.query('select * from usuario where id = $1', [id])
+if (usuario.rows.lenght === 0){
+    return reply.status(400).send({
+        message: "Usuário não existe!"
+    })
+}
+
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2, email = $4 WHERE id = $3', [body.nome, body.senha, id, body.email]);
+    reply.status(201).send({message: `usuario ${body.nome} alterado!`})
 })
 
-servidor.delete("/usuarios/:id", async (request,reply) => {
-    const id = request.params.id;
-    const resultado = await sql.query('UPDATE FROM USUARIO where id= $1', [id])
-    console.log(resultado);
-    reply.status(200).send({message:'Usuário deletado!'})
-})
 
+
+servidor.delete('/usuarios/:id', async (request, reply) => {
+    const id = request.params.id
+    const resultado = await sql.query('DELETE FROM usuario where id = $1', [id])
+    reply.status(200).send({message: 'Usuário Deletado!'})
+})
 
 servidor.listen({
-    port:3000
+    port: 3000
 })
